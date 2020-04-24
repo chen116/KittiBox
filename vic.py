15d204f55ee580d4279693a7733cffc3e8a15f47
vic <chen116@usc.edu>
2020-04-23 20:36:12 -0700

    a

--- vic.py
+++ vic.py
@@ -125,7 +125,13 @@ def main(_):
     else:
         logging.info("Using weights found in {}".format(FLAGS.logdir))
         logdir = FLAGS.logdir
+    tf.reset_default_graph()
 
+    # Create some variables.
+    v1 = tf.get_variable("v1", shape=[3])
+    v2 = tf.get_variable("v2", shape=[5])
+
+    # Add ops to save and restore all the variables.
     saver = tf.train.Saver()
     with tf.Session() as sess:
     # Restore variables from disk.
