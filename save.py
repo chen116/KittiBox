1f2c1ee247eac71e7f3191683886ea13531aedc9
vic <chen116@usc.edu>
2020-04-23 20:24:04 -0700

    a

---
+++ save.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Marvin Teichmann
+
+
+"""
+Detects Cars in an image using KittiBox.
+
+Input: Image
+Output: Image (with Cars plotted in Green)
+
+Utilizes: Trained KittiBox weights. If no logdir is given,
+pretrained weights will be downloaded and used.
+
+Usage:
+python demo.py --input_image data/demo.png [--output_image output_image]
+                [--logdir /path/to/weights] [--gpus 0]
+
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+import logging
+import os
+import sys
+
+import collections
+
+# configure logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.INFO,
+                    stream=sys.stdout)
+
+# https://github.com/tensorflow/tensorflow/issues/2034#issuecomment-220820070
+import numpy as np
+from functools import partial
+
+# save np.load
+np_load_old = partial(np.load)
+
+# modify the default parameters of np.load
+np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+import scipy as scp
+import scipy.misc
+import tensorflow as tf
+
+
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+sys.path.insert(1, 'incl')
+from utils import train_utils as kittibox_utils
+
+try:
+    # Check whether setup was done correctly
+    import tensorvision.utils as tv_utils
+    import tensorvision.core as core
+except ImportError:
+    # You forgot to initialize submodules
+    logging.error("Could not import the submodules.")
+    logging.error("Please execute:"
+                  "'git submodule update --init --recursive'")
+    exit(1)
+
+
+flags.DEFINE_string('logdir', None,
+                    'Path to logdir.')
+flags.DEFINE_string('input_image', None,
+                    'Image to apply KittiBox.')
+flags.DEFINE_string('output_image', None,
+                    'Image to apply KittiBox.')
+
+
+default_run = 'KittiBox_pretrained'
+weights_url = ("ftp://mi.eng.cam.ac.uk/"
+               "pub/mttt2/models/KittiBox_pretrained.zip")
+
+
+def maybe_download_and_extract(runs_dir):
+    logdir = os.path.join(runs_dir, default_run)
+
+    if os.path.exists(logdir):
+        # weights are downloaded. Nothing to do
+        return
+
+    if not os.path.exists(runs_dir):
+        os.makedirs(runs_dir)
+
+    import zipfile
+    download_name = tv_utils.download(weights_url, runs_dir)
+
+    logging.info("Extracting KittiBox_pretrained.zip")
+
+    zipfile.ZipFile(download_name, 'r').extractall(runs_dir)
+
+    return
+
+
+def main(_):
+    tv_utils.set_gpus_to_use()
+
+    if FLAGS.input_image is None:
+        logging.error("No input_image was given.")
+        logging.info(
+            "Usage: python demo.py --input_image data/test.png "
+            "[--output_image output_image] [--logdir /path/to/weights] "
+            "[--gpus GPUs_to_use] ")
+        exit(1)
+
+    if FLAGS.logdir is None:
+        # Download and use weights from the MultiNet Paper
+        if 'TV_DIR_RUNS' in os.environ:
+            runs_dir = os.path.join(os.environ['TV_DIR_RUNS'],
+                                    'KittiBox')
+        else:
+            runs_dir = 'RUNS'
+        maybe_download_and_extract(runs_dir)
+        logdir = os.path.join(runs_dir, default_run)
+    else:
+        logging.info("Using weights found in {}".format(FLAGS.logdir))
+        logdir = FLAGS.logdir
+
+    # Loading hyperparameters from logdir
+    hypes = tv_utils.load_hypes_from_logdir(logdir, base_path='hypes')
+
+    logging.info("Hypes loaded successfully.")
+
+    # Loading tv modules (encoder.py, decoder.py, eval.py) from logdir
+    modules = tv_utils.load_modules_from_logdir(logdir)
+    logging.info("Modules loaded successfully. Starting to build tf graph.")
+
+    # Create tf graph and build module.
+    with tf.Graph().as_default():
+        # Create placeholder for input
+        image_pl = tf.placeholder(tf.float32)
+        image = tf.expand_dims(image_pl, 0)
+
+        # build Tensorflow graph using the model from logdir
+        prediction = core.build_inference_graph(hypes, modules,
+                                                image=image)
+
+        logging.info("Graph build successfully.")
+
+        # Create a session for running Ops on the Graph.
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        
+
+        # Load weights from logdir
+        core.load_weights(logdir, sess, saver)
+        logging.info("Weights loaded successfully.")
+        save_path = saver.save(sess, "./saved_model.ckpt")
+
+
+    exit(1)
+
+    
+    np.load = np_load_old
+if __name__ == '__main__':
+    tf.app.run()
